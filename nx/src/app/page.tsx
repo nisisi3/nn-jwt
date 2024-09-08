8e1711ed8a27ffa36517d878f33c0369eeea007f
nisisi3 <trangiabaotran19@gmail.com>
2024-09-08 06:05:01 +0000

    init

---
+++ nx/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button className="w-fit m-9 text-wrap" variant={"secondary"}>
+        I am the bone of my sword
+      </Button>
+    </main>
+  );
+}
